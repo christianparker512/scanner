1d7aeca108a8beeda59e7a91176b29087c06f613
Christian Parker <christianparker512@gmail.com>
2021-09-06 12:10:50 -0500

    check for invalid year of birth

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -10,16 +10,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); //handle next line character (enter key);
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2021 - yearOfBirth;
-        //can get some info from the keyboard and use it withthe nextLine();
-        System.out.println("Your name is " + name + " and you are " + age);
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); //handle next line character (enter key);
 
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
+            //can get some info from the keyboard and use it with the nextLine();
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + " and you are " + age);
+            } else {
+                System.out.println("Invalid Year of Birth");
+            }
+        } else {
+            System.out.println("unable to parse the year of birth");
+        }
         scanner.close();
-
     }
 }

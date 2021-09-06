01d5ac027d1404b3e060e33017f901ed55870e26
Christian Parker <christianparker512@gmail.com>
2021-09-06 11:52:20 -0500

    fixed the name

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -8,10 +8,16 @@ public class Main {
 	// scanner is a built in class to get user input
         //creating a new instance of the object scanner
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); //handle next line character (enter key);
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
-
-        System.out.println("Your name is " + name);
+        int age = 2021 - yearOfBirth;
+        //can get some info from the keyboard and use it withthe nextLine();
+        System.out.println("Your name is " + name + " and you are " + age);
 
         scanner.close();
 
